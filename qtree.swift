77f356ecb5b3dd47ef0ad9a628b7979ae79c8253
Jason Aeschliman <j.aeschliman@gmail.com>
2015-11-27 17:42:53 -0800

    initial

---
+++ qtree.swift
@@ -0,0 +1,240 @@
+//
+//  qtree.swift
+//  quadtree
+//
+//  Created by Jason Aeschliman on 11/26/15.
+//  Copyright © 2015 Jason Aeschliman. All rights reserved.
+//
+
+import CoreGraphics
+import AppKit
+
+extension CGRect {
+    var rightEdge : CGFloat {
+        return self.origin.x + self.width
+    }
+    var leftEdge : CGFloat {
+        return self.origin.x
+    }
+    var topEdge : CGFloat {
+        return self.origin.y
+    }
+    var bottomEdge : CGFloat {
+        return self.origin.y + self.height
+    }
+}
+
+protocol P {
+    var position : CGPoint { get }
+    var frame    : CGRect  { get }
+}
+
+extension Circle : P {}
+
+private let max = 3
+
+struct Node {
+    let width  : CGFloat
+    let height : CGFloat
+    let center : CGPoint
+    
+    var split : Bool = false
+    var items : [P] = []
+    var subnodes : [Node] = []
+    
+    var a : Node { get { return subnodes[0] } set(n) { subnodes[0] = n } }
+    var b : Node { get { return subnodes[1] } set(n) { subnodes[1] = n } }
+    var c : Node { get { return subnodes[2] } set(n) { subnodes[2] = n } }
+    var d : Node { get { return subnodes[3] } set(n) { subnodes[3] = n } }
+    
+    func hasRoom() -> Bool {
+        if width < 5 || height < 5 {
+            return true
+        }
+        
+        return items.count < max
+    }
+    
+    func draw() {
+        if split {
+            let w = width / 2.0
+            let h = height / 2.0
+            
+            do {
+                let path = NSBezierPath()
+                path.moveToPoint(CGPoint(x : center.x + 0, y : center.y + h))
+                path.lineToPoint(CGPoint(x : center.x + 0, y : center.y - h))
+                path.stroke()
+            }
+            
+            do {
+                let path = NSBezierPath()
+                path.moveToPoint(CGPoint(x : center.x + w, y : center.y + 0))
+                path.lineToPoint(CGPoint(x : center.x - w, y : center.y + 0))
+                path.stroke()
+            }
+            
+            for subnode in subnodes {
+                subnode.draw()
+            }
+        }
+    }
+    
+    /*
+    a b
+    c d
+    */
+    mutating func insert(item : P) {
+        if !split && hasRoom() {
+            items.append(item)
+            return
+        }
+        
+        if !split {
+            items.append(item)
+            splitNode()
+            return
+        }
+        
+        let f = item.frame
+        
+        if f.rightEdge < center.x {
+            if f.bottomEdge < center.y {
+                //a
+                a.insert(item)
+            } else if f.topEdge > center.y {
+                //c
+                c.insert(item)
+            } else {
+                items.append(item)
+            }
+        } else if f.leftEdge > center.x {
+            if f.bottomEdge < center.y {
+                //b
+                b.insert(item)
+            } else if f.topEdge > center.y {
+                //d
+                d.insert(item)
+            } else {
+                items.append(item)
+            }
+        } else {
+            items.append(item)
+        }
+        
+        
+        /*let p = item.position
+        
+        switch (p.x < center.x, p.y < center.y){
+        case (true,  true):  a.insert(item)
+        case (true,  false): c.insert(item)
+        case (false, true):  b.insert(item)
+        case (false, false): d.insert(item)
+        } */
+        
+    }
+    
+    func combineWith(other : P, @noescape block: (P,P)->()) {
+        for item in items {
+            block(other, item)
+        }
+        guard split else { return }
+        a.combineWith(other, block: block)
+        b.combineWith(other, block: block)
+        c.combineWith(other, block: block)
+        d.combineWith(other, block: block)
+    }
+    
+    func possibleCollisions(@noescape block: (P,P)->()) {
+        for i in 0..<items.count {
+            let outer = items[i]
+            for j in (i+1)..<items.count {
+                let inner = items[j]
+                block(outer, inner)
+            }
+            if split {
+                a.combineWith(outer, block: block)
+                b.combineWith(outer, block: block)
+                c.combineWith(outer, block: block)
+                d.combineWith(outer, block: block)
+            }
+        }
+        if split {
+            a.possibleCollisions(block)
+            b.possibleCollisions(block)
+            c.possibleCollisions(block)
+            d.possibleCollisions(block)
+        }
+    }
+    
+    
+    func doItems(p : CGPoint, block: ((P) -> ())) {
+        
+        items.forEach(block)
+        
+        if !split {
+            return
+        }
+        
+        if p.x < center.x {
+            if p.y < center.y {
+                //a
+                a.doItems(p, block: block)
+            } else {
+                //c
+                c.doItems(p, block: block)
+            }
+        } else {
+            if p.y < center.y {
+                //b
+                b.doItems(p, block: block)
+            } else {
+                //d
+                d.doItems(p, block: block)
+            }
+        }
+    }
+    
+    mutating func splitNode() {
+        guard split == false else {
+            fatalError("what???")
+        }
+        split = true
+        
+        let w = width / 4.0
+        let h = height / 4.0
+        
+        let ca = CGPoint(x: center.x - w, y: center.y - h)
+        let cb = CGPoint(x: center.x + w, y: center.y - h)
+        let cc = CGPoint(x: center.x - w, y: center.y + h)
+        let cd = CGPoint(x: center.x + w, y: center.y + h)
+        
+        let w2 = width / 2.0
+        let h2 = height / 2.0
+        subnodes.append(Node(width: w2, height: h2, center: ca, split: false, items: [], subnodes: []))
+        subnodes.append(Node(width: w2, height: h2, center: cb, split: false, items: [], subnodes: []))
+        subnodes.append(Node(width: w2, height: h2, center: cc, split: false, items: [], subnodes: []))
+        subnodes.append(Node(width: w2, height: h2, center: cd, split: false, items: [], subnodes: []))
+        
+        let previous = items
+        items.removeAll()
+        for item in previous {
+            insert(item)
+        }
+    }
+}
+
+class QTree {
+    var root : Node = Node(width: 0, height: 0, center: CGPointZero, split: false, items: [], subnodes: [])
+    
+    func reset(rect : CGRect) {
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        root = Node(width: rect.width, height: rect.height, center: center, split: false, items: [], subnodes: [])
+    }
+    
+    func insert(item : P) {
+        root.insert(item)
+    }
+    
+}
+
---
+++ quadtree/CirclesView.swift
@@ -0,0 +1,248 @@
+//
+//  CirclesView.swift
+//  quadtree
+//
+//  Created by Jason Aeschliman on 11/26/15.
+//  Copyright © 2015 Jason Aeschliman. All rights reserved.
+//
+
+import AppKit
+
+extension CGPoint {
+    func dot(other: CGPoint) -> CGFloat {
+        return (self.x * other.x) + (self.y * other.y)
+    }
+    func sub(other: CGPoint) -> CGPoint {
+        return CGPoint(x: self.x - other.x, y: self.y - other.y)
+    }
+    func add(other: CGPoint) -> CGPoint {
+        return CGPoint(x: self.x + other.x, y: self.y + other.y)
+    }
+    func mag() -> CGFloat {
+        return hypot(self.x, self.y)
+    }
+    func heading() -> CGFloat {
+        return atan2(self.y, self.x)
+    }
+    func mul(n: CGFloat) -> CGPoint {
+        return CGPoint(x: self.x * n, y: self.y * n)
+    }
+    
+}
+
+class Circle {
+    
+    init(radius: CGFloat, position: CGPoint, delta: CGPoint) {
+        self.radius = radius
+        self.position = position
+        self.delta = delta
+        self.mass = 0.1 * radius
+    }
+    
+    let mass : CGFloat
+    let radius : CGFloat
+    var position : CGPoint
+    var delta : CGPoint
+    
+    func collidesWith(other: Circle) -> Bool {
+        let dx = other.position.x - self.position.x
+        let dy = other.position.y - self.position.y
+        let a = dx * dx
+        let b = dy * dy
+        let c = sqrt(a + b)
+        return c < (other.radius + self.radius)
+    }
+    
+    var drawFill = false
+    
+    var frame : CGRect {
+        let d = radius * 2
+        return CGRect(x: position.x - radius, y: position.y - radius, width: d, height: d)
+    }
+    
+    func draw() {
+        NSBezierPath(ovalInRect: frame).stroke()
+        if drawFill {
+            NSColor.cyanColor().setFill()
+        } else {
+            NSColor.whiteColor().setFill()
+        }
+        NSBezierPath(ovalInRect: frame).fill()
+    }
+    
+    func update(world : CGSize) {
+        var x = position.x + delta.x
+        var y = position.y + delta.y
+        
+        var hit = false
+        
+        if x < 0 {
+            x = 0
+            delta.x *= -1
+            hit = true
+        } else if x > world.width {
+            x = world.width
+            delta.x *= -1
+            hit = true
+        }
+        
+        if y < 0 {
+            y = 0
+            delta.y *= -1
+            hit = true
+        } else if y > world.height {
+            y = world.height
+            delta.y *= -1
+            hit = true
+        }
+        
+        //damp velocity to counter crappy physics
+        if hit {
+            delta = delta.mul(0.8)
+        }
+        
+        position = CGPoint(x: x, y: y)
+        drawFill = hit
+    }
+    
+    func collide(other : Circle) {
+        // cribbed from: https://processing.org/examples/circlecollision.html
+        
+        let b = other.position.sub(self.position)
+        let theta = b.heading()
+        let sine = sin(theta)
+        let cosine = cos(theta)
+        
+        var bTemp = [CGPointZero, CGPointZero]
+        
+        bTemp[1].x = cosine * b.x + sine * b.y
+        bTemp[1].y = cosine * b.y - sine * b.x
+        
+        var vTemp = [CGPointZero, CGPointZero]
+        
+        vTemp[0].x = cosine * delta.x + sine * delta.y
+        vTemp[0].y = cosine * delta.y + sine * delta.x
+        vTemp[1].x = cosine * other.delta.x + sine * other.delta.y
+        vTemp[1].y = cosine * other.delta.y + sine * other.delta.x
+        
+        var vFinal = [CGPointZero, CGPointZero]
+        
+        vFinal[0].x = ((mass - other.mass) * vTemp[0].x + 2 * other.mass * vTemp[1].x) / (mass + other.mass)
+        vFinal[0].y = vTemp[0].y
+        
+        vFinal[1].x = ((other.mass - mass) * vTemp[1].x + 2 * mass * vTemp[0].x) / (mass + other.mass)
+        vFinal[1].y = vTemp[1].y
+        
+        //hack to avoid clumping
+        bTemp[0].x += vFinal[0].x
+        bTemp[1].x += vFinal[1].x
+        //bTemp[0].y += vFinal[0].y
+        //bTemp[1].y += vFinal[1].y
+        
+        var bFinal = [CGPointZero, CGPointZero]
+        
+        bFinal[0].x = cosine * bTemp[0].x - sine * bTemp[0].y
+        bFinal[0].y = cosine * bTemp[0].y + sine * bTemp[0].x
+        bFinal[1].x = cosine * bTemp[1].x - sine * bTemp[1].y
+        bFinal[1].y = cosine * bTemp[1].y + sine * bTemp[1].x
+        
+        other.position = position.add(bFinal[1])
+        position = position.add(bFinal[0])
+        
+        let vx = cosine * vFinal[0].x - sine * vFinal[0].y
+        let vy = cosine * vFinal[0].y + sine * vFinal[0].x
+        let ovx = cosine * vFinal[1].x - sine * vFinal[1].y
+        let ovy = cosine * vFinal[1].y + sine * vFinal[1].x
+        
+        delta = CGPoint(x: vx, y: vy)
+        other.delta = CGPoint(x: ovx, y: ovy)
+    }
+    
+    var hit = false
+}
+
+
+class CirclesView : NSView {
+    
+    var circles = [Circle]()
+    let qtree   = QTree()
+    
+    override func awakeFromNib() {
+        let count = 100
+        let max_radius : Int32 = 15
+        let min_radius : Int32 = 5
+        
+        for _ in 0..<count {
+            let r = CGFloat((rand() % (max_radius - min_radius)) + min_radius)
+            let x = CGFloat(rand()) % bounds.width
+            let y = CGFloat(rand()) % bounds.height
+            let mx = 2
+            let dx = CGFloat((rand() % Int32(2 * mx)) - mx)
+            let dy = CGFloat((rand() % Int32(2 * mx)) - mx)
+            
+            let d = CGPoint(x: dx, y: dy)
+            
+            let c = Circle(radius: r, position: CGPoint(x: x, y: y), delta: d)
+            circles.append(c)
+        }
+        
+        let timer = NSTimer(timeInterval: 0.03, target: self, selector: "tick", userInfo: nil, repeats: true)
+        NSRunLoop.currentRunLoop().addTimer(timer, forMode: NSDefaultRunLoopMode)
+    }
+    
+    func tick() {
+        
+        qtree.reset(self.bounds)
+        
+        for c in circles {
+            c.update(self.bounds.size)
+            c.drawFill = false
+            qtree.insert(c)
+        }
+        
+        qtree.root.possibleCollisions {
+            a, b in
+            let a = a as! Circle
+            let b = b as! Circle
+            
+            if a.collidesWith(b) {
+                a.collide(b)
+                a.drawFill = true
+                b.drawFill = true
+            }
+        }
+        
+        self.needsDisplay = true
+    }
+    
+    override func drawRect(dirtyRect: NSRect) {
+        NSColor.whiteColor().setFill()
+        NSRectFill(dirtyRect)
+        NSColor.cyanColor().setFill()
+        NSColor.blackColor().setStroke()
+        
+        qtree.root.draw()
+        
+        NSColor.redColor().setStroke()
+        
+        qtree.root.possibleCollisions {
+            a, b in
+            let a = a as! Circle
+            let b = b as! Circle
+            
+            let path = NSBezierPath()
+            path.moveToPoint(a.position)
+            path.lineToPoint(b.position)
+            path.stroke()
+        }
+        
+        for c in circles {
+            c.draw()
+        }
+        
+    }
+}
+
+
+
+
